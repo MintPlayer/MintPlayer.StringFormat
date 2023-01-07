8a6cf6273c5ee2943457e41823c59c3eb6af8462
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-01-07 01:05:19 +0100

    Add project files.

---
+++ MintPlayer.StringFormat.Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MintPlayer.StringFormat.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var strExample = StringExtensions.FormatAdvanced(@"Hello {0} this is {1:N2} Life should {2:yyyy:global::System.DateTime}", out var anchors, "world", "250", DateTimeOffset.Now);
+        }
+    }
+}
